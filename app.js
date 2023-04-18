0a51cd2a20d3c9e713dd78224a8283962e4e553f
Nagendraharu <nbabu724@gmail.com>
2023-04-18 02:17:45 -0400

    Updated the code to include email in the model for authentication.

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ passport.use(new GoogleStrategy({
 },
 function(accessToken, refreshToken, profile, cb) {
   console.log(profile);
-  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+  User.findOrCreate({ googleId: profile.id, email: profile.emails[0].value }, function (err, user) {
     return cb(err, user);
   });
 }
